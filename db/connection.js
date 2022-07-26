149d06cbaa09e9fc211079611e04037b946d52ff
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-07-26 17:49:55 -0500

    contain access to my database

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+//require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '',
-    database: 'election'
+    password: 'Yhbylnb8',
+    database: 'employee_tracker_db'
 });
 
 module.exports = db;
